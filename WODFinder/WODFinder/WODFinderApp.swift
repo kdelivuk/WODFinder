ca5d079283cbbf53152a90e54362bb27e681e353
Kristijan Delivuk <kdelivuk@gmail.com>
2020-11-09 15:46:48 +0100

    Initial project commit.

---
+++ WODFinder/WODFinder/WODFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WODFinderApp.swift
+//  WODFinder
+//
+//  Created by Kristijan Delivuk on 09/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WODFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
